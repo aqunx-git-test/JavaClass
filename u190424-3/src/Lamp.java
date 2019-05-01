fd850914b9550a342f8092e1f8cebad918179893
LuaGanrqunx <aqunxgm@gmail.com>
2019-05-01 16:31:36 +0800

    from mac

--- u190424-3/src/Lamp.java
+++ u190424-3/src/Lamp.java
@@ -2,13 +2,13 @@
 public class Lamp {
 //	屬性
 //	顏色
-	String color;
+	String color = "White";
 //	目前狀態
 	//考慮分成兩種狀態：電源power，亮度brightness
 	//電源power: On, Off
-	String power;
+	String power = "Off";
 	//亮度brightness
-	String brightness;
+	String brightness = "Dark";
 //	關、夜間、電腦、休閒、閱讀: Dark, Night, Computer, Leisure, Reading
 //	動作（操作）
 //	按（開關）: press
@@ -18,17 +18,13 @@ public class Lamp {
 		}
 		if(brightness.equals("Dark")){
 			brightness = "Night";
-		}
-		if(brightness.equals("Night")){
+		} else if(brightness.equals("Night")){
 			brightness = "Computer";
-		}
-		if(brightness.equals("Computer")){
+		} else if(brightness.equals("Computer")){
 			brightness = "Leisure";
-		}
-		if(brightness.equals("Leisure")){
+		} else if(brightness.equals("Leisure")){
 			brightness = "Reading";
-		}
-		if(brightness.equals("Reading")){
+		} else if(brightness.equals("Reading")){
 			brightness = "Night";
 		}
 	}
@@ -43,4 +39,25 @@ public class Lamp {
 		System.out.println("亮度：**"+ this.brightness+"**");
 	}
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lamp lamp = new Lamp(); 
+		
+		lamp.displayStatus();
+		
+		lamp.press();
+		lamp.displayStatus();
+		lamp.press();
+		lamp.displayStatus();
+		lamp.press();
+		lamp.displayStatus();
+		lamp.press();
+		lamp.displayStatus();
+		lamp.press();
+		lamp.displayStatus();
+		lamp.longPress();
+		lamp.displayStatus();
+		
+
+	}
 }
